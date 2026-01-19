a1f5d611fbdd322531307520a88a97d0510f3dc7
Felix Lima <32372236+felixlimaa@users.noreply.github.com>
2026-01-19 14:31:00 -0300

    google-search.py com pesquisas do YouTube incluídas

--- google-search.py
+++ google-search.py
@@ -1,14 +1,17 @@
 import requests
 from time import sleep
-print("AUTOMAÇÃO DE PESQUISAS NO GOOGLE")
+print("AUTOMAÇÃO DE PESQUISAS")
 keyword = str(input("Digite sua palavra-chave: ")).strip().lower()
+opcoes = [{"nome": "Google", "endpoint": f"http://google.com/complete/search?client=chrome&q={keyword}"}, {"nome": "YouTube", "endpoint": f"https://suggestqueries.google.com/complete/search?client=chrome&ds=yt&q={keyword}"}]
+escolha = int(input("Onde você quer pesquisar?\n[0] Google\n[1] YouTube\nDigite um número: "))
+print(f"Pesquisando {keyword.upper()} no {opcoes[escolha]["nome"].upper()}...")
 alfabeto = {"letras": " abcdefghijklmnopqrstuvwxyz"}
 letras = [l for l in alfabeto['letras']]
 for c in range(len(letras)):
     if letras[c] == " ":
-        api = requests.get(f"http://google.com/complete/search?client=chrome&q={keyword}")
+        api = requests.get(opcoes[escolha]["endpoint"])
     else:
-        api = requests.get(f"http://google.com/complete/search?client=chrome&q={keyword}%{letras[c]}")
+        api = requests.get(f"{opcoes[escolha]["endpoint"]}%{letras[c]}")
     sugestoes = api.json()
     if letras[c] == " ":
         print(f"\nSugestões para {keyword.upper()}:")
